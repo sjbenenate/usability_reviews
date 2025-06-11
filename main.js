b06ac3f5cd9b9d19f942d282cedfbb3bece75c50
sjbenenate <sarahjoybenenate@gmail.com>
2025-06-10 17:16:23 -0700

    production mode environment variable

--- main.js
+++ main.js
@@ -3,4 +3,11 @@ import 'dotenv/config';
 
 const PORT = process.env.PORT || 3000;
 
+const PRODUCTION = process.env.PROD_ENV.toUpperCase() === 'TRUE';
+console.log(PRODUCTION);
+if (PRODUCTION) {
+  console.log('RUNNING IN PRODUCTION MODE');
+} else {
+  console.log('non-production mode');
+}
 server.start(PORT);
