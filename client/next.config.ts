6ba34716c606806aa50ecdc61b19178199c49737
sjbenenate <sarahjoybenenate@gmail.com>
2025-06-13 09:35:26 -0700

    next client reinitialized with tailwind css and typescript

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   async rewrites() {
     const API_DOMAIN = process.env.API_DOMAIN;
     console.log(`USING API_DOMAIN: ${API_DOMAIN}`);
